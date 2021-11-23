96a347285c88153475587f41689b50c45ef836df
jaerith <Aaron.Kendall@gmail.com>
2021-11-23 03:28:16 +0000

    Added more skeleton code.

---
+++ Siwe/Messages/SessionData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siwe.Messages
+{
+    public record SessionData
+    {
+        public SiweMessage? Siwe { get; init; }
+
+        public string? Nonce { get; init; }
+
+        public string? Ens { get; init; }
+    }
+}
