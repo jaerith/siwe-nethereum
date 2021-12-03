b74d4868d0703fc46058a121719517c6d5ce66ef
jaerith <Aaron.Kendall@gmail.com>
2021-12-03 23:28:26 +0000

    Moved SiweMeResult from one project to another.

---
+++ Siwe/Messages/SiweMeResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siwe.Messages
+{
+    public record SiweMeResult
+    {
+        public string? Text { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Ens { get; set; }
+
+        public SiweMeResult()
+        { }
+    }
+}
