c922c3778ed00cdb212469d1a785b366b5df3c7e
jaerith <Aaron.Kendall@gmail.com>
2021-12-11 22:50:34 +0000

    Iniital implementation.

---
+++ siwe-nethereum/RestServices/ISiweRestService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+using siwe.Messages;
+using siwe_rest_service.Models;
+
+namespace siwe_nethereum.RestServices
+{
+    public interface ISiweRestService
+    {
+        public Task<SiweMeResult> GetMe();
+
+        public Task<string> GetNonce();
+
+        public Task<SiweMessageAndText> PostSignIn(SiweMessage message);
+
+        public Task<SiweMeResult> PostSignOut(SiweMessage message);
+
+        public Task<SiweMessageAndText> PutSave(SiweMessage message);
+    }
+}
---
+++ siwe-nethereum/RestServices/SiweRestService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using siwe.Messages;
+using siwe_rest_service.Models;
+
+namespace siwe_nethereum.RestServices
+{
+    public class SiweRestService : ISiweRestService
+    {
+        private readonly string     baseUrl;
+        private readonly HttpClient httpClient;
+
+        public SiweRestService(string providedBaseUrl = null)
+        {
+            baseUrl    = providedBaseUrl ?? "http://localhost:1234";
+            httpClient = new HttpClient();
+
+            httpClient.BaseAddress = new Uri(baseUrl);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public SiweRestService(HttpClient client, string providedBaseUrl = null)
+        {
+            baseUrl    = providedBaseUrl ?? "http://localhost:1234";
+            httpClient = client;
+
+            httpClient.BaseAddress = new Uri(baseUrl);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<SiweMeResult> GetMe()
+        {
+            return await httpClient.GetFromJsonAsync<SiweMeResult>("api/me");
+        }
+
+        public async Task<string> GetNonce()
+        {
+            return await httpClient.GetStringAsync(baseUrl + "api/nonce");
+        }
+
+        public async Task<SiweMessageAndText> PostSignIn(SiweMessage message)
+        {
+            var MsgWithText = new SiweMessageAndText();
+            var response    = await httpClient.PostAsJsonAsync<SiweMessage>("api/signin", message);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                MsgWithText = JsonConvert.DeserializeObject<SiweMessageAndText>(responseContent);
+            }
+
+            return MsgWithText;
+        }
+
+        public async Task<SiweMeResult> PostSignOut(SiweMessage message)
+        {
+            var meResult = new SiweMeResult();
+            var response = await httpClient.PostAsJsonAsync<SiweMessage>("api/signout", message);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                meResult = JsonConvert.DeserializeObject<SiweMeResult>(responseContent);
+            }
+
+            return meResult;
+        }
+
+        public async Task<SiweMessageAndText> PutSave(SiweMessage message)
+        {
+            var MsgWithText = new SiweMessageAndText();
+            var response    = await httpClient.PutAsJsonAsync<SiweMessage>("api/save", message);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                MsgWithText = JsonConvert.DeserializeObject<SiweMessageAndText>(responseContent);
+            }
+
+            return MsgWithText;
+        }
+    }
+}
