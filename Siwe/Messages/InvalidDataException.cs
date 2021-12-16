9868cfaf9b7fcd95c5a4c1d2606ae920569d5695
jaerith <Aaron.Kendall@gmail.com>
2021-12-16 23:08:25 +0000

    Added other exception classes.

---
+++ Siwe/Messages/InvalidDataException.cs
@@ -0,0 +1,11 @@
+﻿namespace siwe.Messages
+{
+    public class InvalidSiweDataException : SiweException
+    {
+        public InvalidSiweDataException() : base()
+        { }
+
+        public InvalidSiweDataException(string message) : base(message)
+        { }
+    }
+}
---
+++ Siwe/Messages/NoNonceException.cs
@@ -0,0 +1,11 @@
+﻿namespace siwe.Messages
+{
+    public class NoNonceException : SiweException
+    {
+        public NoNonceException() : base()
+        { }
+
+        public NoNonceException(string message) : base(message)
+        { }
+    }
+}
