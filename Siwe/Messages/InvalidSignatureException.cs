ea9fca3b1e68ce9e6a84cbab0d35a46ee5188910
jaerith <Aaron.Kendall@gmail.com>
2021-12-16 23:07:52 +0000

    Removed unnecessary references.

--- Siwe/Messages/InvalidSignatureException.cs
+++ Siwe/Messages/InvalidSignatureException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace siwe.Messages
+﻿namespace siwe.Messages
 {
     public class InvalidSignatureException : SiweException
     {
