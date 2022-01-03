e9bede1079615acb60db0f9bdef745c2473cc367
jaerith <Aaron.Kendall@gmail.com>
2022-01-03 17:51:52 +0000

    Added enums/interfaces to be used for anchoring CACAO objects.

---
+++ Siwe/Anchor/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace siwe.Anchor
+{
+    /**
+     * Possible anchor types that this library supports.
+     */
+    [DataContract(Namespace = "http://siwe.metdatadojo.wordpress.com", Name = "AnchorProviderType")]
+    public enum AnchorProviderType
+    {
+        [EnumMember]
+        IPFS = 1,
+
+        [EnumMember]
+        Ceramic,
+
+        [EnumMember]
+        Polygon,
+
+        [EnumMember]
+        Other
+
+    }
+}
---
+++ Siwe/Anchor/IAnchorProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using siwe.Cacao;
+
+namespace siwe.Anchor
+{
+    public interface IAnchorProvider
+    {
+        AnchorProviderType GetType();
+
+        string Anchor(CacaoMessage msg);
+    }
+}
