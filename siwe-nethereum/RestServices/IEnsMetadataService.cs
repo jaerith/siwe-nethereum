697cb74b9d6937dd89497abe57f55ae0ff5d84f8
jaerith <Aaron.Kendall@gmail.com>
2022-01-19 23:44:21 +0000

    Added interface for new REST service.

---
+++ siwe-nethereum/RestServices/IEnsMetadataService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace siwe_nethereum.RestServices
+{
+    public interface IEnsMetadataService
+    {
+        public Task<string> GetEnsAvatarUrl(string ensName);
+    }
+}
