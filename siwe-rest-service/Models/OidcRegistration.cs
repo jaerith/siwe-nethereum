60c538d5a05c2b759176222a34772a4b7322743d
jaerith <Aaron.Kendall@gmail.com>
2022-06-06 20:03:00 -0400

    Added class for receiving OIDC registration payloads.

---
+++ siwe-rest-service/Models/OidcRegistration.cs
@@ -0,0 +1,15 @@
+﻿namespace siwe_rest_service.Models
+{
+    public class OidcRegistration
+    {
+        public string? client_id { get; set; }
+
+        public string? client_secret { get; set; }
+
+        public string? registration_access_token { get; set; }
+
+        public string? registration_client_uri { get; set; }
+
+        public List<string>? redirect_uris { get; set; }
+    }
+}
