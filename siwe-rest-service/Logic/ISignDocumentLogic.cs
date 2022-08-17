83169761106e7cc92ef503183034b13d96d9174c
jaerith <Aaron.Kendall@gmail.com>
2022-08-17 17:11:04 -0400

    Initial revision.

---
+++ siwe-rest-service/Logic/ISignDocumentLogic.cs
@@ -0,0 +1,15 @@
+﻿namespace siwe_rest_service.Logic
+{
+    public interface ISignDocumentLogic
+    {
+        ushort RegisterDocument(string multiHash);
+
+        string GetDocumentHash(ushort docId);
+
+        bool IsDocumentSigned(string user, ushort docId);
+
+        DateTime GetDocumentSigningDateTime(string user, ushort docId);
+
+        void SignDocument(string user, ushort docId, string signature);
+    }
+}
