c24ee1b0b618e81d698c6d597a1b39bf7b021320
jaerith <Aaron.Kendall@gmail.com>
2022-08-18 19:07:04 -0400

    Initial revision.

---
+++ siwe-rest-service/Logic/EthereumSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace siwe_rest_service.Logic
+{
+    public class EthereumSettings
+    {
+        public string Url { get; set; }
+        public string AccountPrivateKey { get; set; }
+    }
+}
