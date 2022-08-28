ae5782023d8a7fff707e6054ef1be7596b9cce6e
jaerith <Aaron.Kendall@gmail.com>
2022-08-28 15:24:56 -0400

    Initial revision.

---
+++ siwe-rest-service/Models/DocSignData.cs
@@ -0,0 +1,18 @@
+﻿namespace siwe_rest_service.Models
+{
+    public class DocSignData
+    {
+        public string? DocumentHash { get; set; }
+
+        public ushort? DocId { get; set; }
+
+        public string? DocSigner { get; set; }
+
+        public string? DocSignSignature { get; set; }
+
+        public DateTime? DocSignDateTime { get; set; }
+
+        public DocSignData()
+        { }
+    }
+}
---
+++ siwe-rest-service/Models/SiweMessageAndDocSign.cs
@@ -0,0 +1,16 @@
+﻿using Nethereum.Siwe.Core;
+
+using siwe.Messages;
+
+namespace siwe_rest_service.Models
+{
+    public class MySiweMessageAndDocSign : MySiweMessage
+    {
+        public DocSignData? docSignData = null;
+
+        public MySiweMessageAndDocSign(DocSignData data) : base()
+        { 
+            docSignData = data;
+        }
+    }
+}
