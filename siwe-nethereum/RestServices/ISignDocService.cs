d47a7b3cd19fcd3b975f9c2bf0575caa3fbb4ce9
jaerith <Aaron.Kendall@gmail.com>
2022-09-02 13:54:19 -0400

    Initial revision.

---
+++ siwe-nethereum/RestServices/ISignDocService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+using siwe.Messages;
+using siwe_rest_service.Models;
+
+namespace siwe_nethereum.RestServices
+{
+    public interface ISignDocService
+    {
+        public Task<DocSignData> GetSignatureDateTime(MySiweMessage message);
+
+        public Task<DocSignData> PostDocumentRegistration(MySiweMessage message);
+
+        public Task<DocSignData> PutDocumentSignature(MySiweMessage message);
+    }
+}
